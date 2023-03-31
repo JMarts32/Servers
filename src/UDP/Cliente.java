332adeafe0dc36df5f1adeedbc31c8c74e003368
Daniel Osorio <d.osorioc2@uniandes.edu.co>
2023-03-31 01:23:49 -0500

    Changes in servidor UDP

--- src/UDP/Cliente.java
+++ src/UDP/Cliente.java
@@ -1,43 +1,61 @@
 package UDP;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
 
-public class Cliente extends Thread{
+public class Cliente extends Thread {
 
-        private static final String SERVER_IP = "192.168.184.133";
-        private static final int SERVER_PORT = 9999;
-        private int id;
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 9999;
+    private int id;
 
 
-        public Cliente(int id){
-            this.id = id;
-        }
+    public Cliente(int id) {
+        this.id = id;
+    }
 
-        public void run(){
-            try {
+    public void run() {
+        try {
 
-                // Se crea el socket UDP
-                DatagramSocket socketUDP = new DatagramSocket();
-                // Se prepara el mensaje
-                String mensaje = "Hola servidor soy el cliente " + id;
-                // Se prepara el datagrama para enviar el mensaje
-                DatagramPacket paquete = new DatagramPacket(mensaje.getBytes(), mensaje.getBytes().length, InetAddress.getByName(SERVER_IP), SERVER_PORT);
-                // Se envía el datagrama
-                socketUDP.send(paquete);
-                // Se prepara el datagrama para recibir la respuesta
-                byte[] bufer = new byte[1024];
-                DatagramPacket respuesta = new DatagramPacket(bufer, bufer.length);
-                // Se recibe la respuesta
+            // Se crea el socket UDP
+            DatagramSocket socketUDP = new DatagramSocket();
+            // Se envia el id del cliente
+            byte[] mensaje = ByteBuffer.allocate(4).putInt(id).array(); // Se convierte el id a bytes, se pone 4 porque es el tamaño de un int
+            // Se prepara el datagrama para enviar el mensaje
+            DatagramPacket paquete = new DatagramPacket(mensaje, mensaje.length, InetAddress.getByName(SERVER_IP), SERVER_PORT);
+            // Se envía el datagrama
+            socketUDP.send(paquete);
+
+            // Se prepara el datagrama para recibir el numero de partes
+            byte[] bufer = new byte[4];
+            DatagramPacket respuesta = new DatagramPacket(bufer, bufer.length);
+            // Se recibe el numero de partes
+            socketUDP.receive(respuesta);
+            // Se obtiene el numero de partes
+            int numChunks = ByteBuffer.wrap(respuesta.getData()).getInt();
+            // Se obtienen las partes
+            for (int i = 0; i < numChunks; i++) {
+                // Se prepara el datagrama para recibir la parte
+                bufer = new byte[65507];
+                respuesta = new DatagramPacket(bufer, bufer.length);
+                // Se establece un timeout de 5 segundos para que no se quede esperando indefinidamente
+                socketUDP.setSoTimeout(5000);
+                // Se recibe la parte
                 socketUDP.receive(respuesta);
-                // Se cierra el socket
-                socketUDP.close();
-                // Se muestra la respuesta
-                System.out.println(new String(respuesta.getData()));
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
+                // Se obtiene la parte
+                byte[] chunk = respuesta.getData();
+                // TODO: Se escribe la parte en el archivo
+                // writeLog(new String(chunk, 0, chunk.length));
             }
 
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
 }
--- src/UDP/Servidor.java
+++ src/UDP/Servidor.java
@@ -1,11 +1,21 @@
 package UDP;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Servidor {
 
     private static final int PORT = 9999;
-    private static final int BUFFER_SIZE = 1024; //
+    private static final int BUFFER_SIZE = 64;
+    private static final String LOG_DIRECTORY = "logs/";
+    private static final String DATE_FORMAT = "yyyy-MM-dd-HH-mm-ss";
+    private static Scanner input;
+    private static String nombreArchivoLog;
+    private static FileWriter archivoLog;
 
     public static void main(String[] args) {
 
@@ -13,7 +23,53 @@ public class Servidor {
             // Se inicializa el servidor UDP en el puerto establecido
             DatagramSocket socketUDP = new DatagramSocket(PORT);
             System.out.println("Servidor UDP iniciado en el puerto " + PORT);
+            // Se pregunta al usuario cual archivo quiere enviar
+            System.out.println("¿Que archivo quieres enviar?");
+            System.out.println("1. Archivo 100MB");
+            System.out.println("2. Archivo 250MB");
+            input = new Scanner(System.in);
+            int opcion = input.nextInt();
+            String nombreArchivo = "";
+            switch (opcion) {
+                case 1:
+                    nombreArchivo = "100MB.txt";
+                    break;
+                case 2:
+                    nombreArchivo = "250MB.txt";
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+            // Se pregunta al usuario cual es el tamaño de los mensajes en que se va a dividir el archivo
+            System.out.println("¿Cual es el tamaño de los mensajes en que se va a dividir el archivo?");
+            int messageSize = input.nextInt();
+            // Se verifica que el buffer tenga un tamaño entre 1 y 64000
+            if (BUFFER_SIZE < 1 || BUFFER_SIZE > 64000) {
+                System.out.println("El tamaño del buffer debe estar entre 1 y 64000");
+                return;
+            }
+            // Se obtiene la fecha y hora actual
+            Date fechaActual = new Date();
+            // Se define el formato de la fecha y hora
+            SimpleDateFormat formato = new SimpleDateFormat(DATE_FORMAT);
+            // Se formatea la fecha y hora según el formato definido
+            String fechaFormateada = formato.format(fechaActual);
+            // Se utiliza la fecha formateada para nombrar el archivo log
+            String nombreArchivoLog = fechaFormateada + "-log";
+            // Crea el directorio si no existe
+            File directory = new File(LOG_DIRECTORY);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            // Se crea el archivo del log del servidor
+            FileWriter archivoLog = new FileWriter(LOG_DIRECTORY + nombreArchivoLog + ".txt");
+            // Se escribe en el archivo de log el nombre del archivo que se va a enviar y el tamaño de los mensajes
+            archivoLog.write("Archivo a enviar: " + nombreArchivo + " Tamaño de los mensajes: " + messageSize);
+            // Se cierra el archivo de log
+            archivoLog.close();
 
+            // Se crea una conexión para cada cliente que se conecte
             while (true) {
                 // Se prepara el paquete para recibir los datos
                 byte[] bufer = new byte[BUFFER_SIZE];
@@ -22,7 +78,7 @@ public class Servidor {
                 // Se recibe la petición del cliente
                 socketUDP.receive(peticion);
                 // Se procesa la petición del cliente
-                ConexionCliente conexion = new ConexionCliente(peticion);
+                ConexionCliente conexion = new ConexionCliente(peticion, nombreArchivo, nombreArchivoLog, messageSize);
                 conexion.start();
             }
 
