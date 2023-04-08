8b94198b3eeef2a3f5871b587b90b37e96c1df87
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2023-04-08 00:56:14 -0500

    Comenzando pruebas (#1)

--- src/TCP/Cliente/Cliente.java
+++ src/TCP/Cliente/Cliente.java
@@ -1,4 +1,44 @@
 package TCP.Cliente;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Cliente {
+    public static void main(String[] args) {
+        try{
+            String serverAddress = "localhost";
+            int serverPort = 12345;
+
+            Socket socket = new Socket(serverAddress, serverPort);
+            System.out.println("Conectando con el servidor " + serverAddress + ":" + serverPort);
+
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter outputWriter = new PrintWriter(outputStream, true);
+
+            String fileName = "archivo.txt";
+            outputWriter.println(fileName);
+
+            File file = new File(fileName);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            fileInputStream.close();
+            System.out.println("Archivo " + fileName + " enviado");
+            String response = inputReader.readLine();
+            System.out.println(response);
+
+            socket.close();
+            System.out.println("Desconectado del servidor");
+
+        }catch (Exception e) {e.printStackTrace();}
+
+    }
 }
--- src/TCP/Servidor/Servidor.java
+++ src/TCP/Servidor/Servidor.java
@@ -1,4 +1,46 @@
 package TCP.Servidor;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+    public static void main(String[] args) {
+        try{
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("Servidor iniciado");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
+
+                InputStream inputStream = clientSocket.getInputStream();
+                BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                OutputStream outputStream = clientSocket.getOutputStream();
+                PrintWriter outputWriter = new PrintWriter(outputStream, true);
+
+                String fileName = inputReader.readLine();
+                System.out.println("Recibiendo archivo " + fileName);
+
+                File file = new File(fileName);
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                fileOutputStream.close();
+                System.out.println("Archivo " + fileName + " recibido");
+                outputWriter.println("Archivo " + fileName + " recibido correctamente");
+
+                clientSocket.close();
+                System.out.println("Cliente desconectado");
+            }
+
+        }catch (Exception e ){e.printStackTrace();}
+
+    }
 }
