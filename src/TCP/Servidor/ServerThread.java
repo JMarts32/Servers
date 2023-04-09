8ec6d1d72d1cbba5bff5db9d8367dcd81d75770b
JMarts32 <j.santosa@uniandes.edu.co>
2023-04-09 15:58:56 -0500

    Server Main and Server thread finished, not tested yet

--- src/TCP/Servidor/ServerThread.java
+++ src/TCP/Servidor/ServerThread.java
@@ -1,32 +1,97 @@
 package TCP.Servidor;
 
-import java.io.File;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.time.Instant;
 
 public class ServerThread extends Thread{
 
-    private Socket serverSocket;
+    private Socket clientSocket;
     private int threadId;
-    private ServerSocket communicationSocket;
+    private ServerSocket communicationServerSocket;
     private int randomIndex;
     private File[] files;
     private File folder;
+    private File logFile;
+    private static Object lock = new Object();
 
-    public ServerThread(Socket sc, int id, ServerSocket cms, int index, File[] f, File fol){
+    public ServerThread(Socket sc, int id, ServerSocket cms, int index, File[] f, File fol, File lf){
         try{
-            this.serverSocket = sc;
+            this.clientSocket = sc;
             this.threadId = id;
-            this.communicationSocket = cms;
+            this.communicationServerSocket = cms;
             this.randomIndex = index;
             this.files = f;
             this.folder = fol;
+            this.logFile = lf;
         }catch (Exception e){e.printStackTrace();}
     }
 
     @Override
     public void run(){
-        
+        try (InputStream is = clientSocket.getInputStream()){
+            DataInputStream dis = new DataInputStream(is);
+
+            String filename = files[randomIndex].getName();
+            File file = new File(folder + "/" + filename);
+
+            // calculate the hash of the file
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+
+            // Comienza el proceso de envio del archivo al cliente
+            OutputStream os = clientSocket.getOutputStream();
+            FileInputStream fis = new FileInputStream(file);
+
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+
+            Instant start = Instant.now();
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+                digest.update(buffer, 0,bytesRead);
+            }
+            Instant end = Instant.now();
+
+            synchronized (lock){
+                // Se escriben los logs dentro del archivo
+                FileWriter writer = new FileWriter(logFile);
+                writer.write("Archivo enviado: " + filename + "\n");
+                writer.write("Tamaño de archivo: " + file.length() + " bytes\n");
+                writer.write("Dirección IP del cliente: " + clientSocket.getInetAddress().toString() + "\n");
+                writer.write("Éxito de entrega: " + true + "\n");
+                writer.write("Tiempo de transferencia: " + (end.toEpochMilli() - start.toEpochMilli()) + " ms\n");
+                writer.flush();
+                writer.close();
+            }
+
+            // Close streams
+            dis.close();
+            is.close();
+            os.close();
+            fis.close();
+            clientSocket.close();
+
+            Socket communicationSocket;
+            PrintWriter out;
+
+            communicationSocket = communicationServerSocket.accept();
+            out = new PrintWriter(communicationSocket.getOutputStream());
+
+            // Calculates the final hash and sends it to the client
+            byte[] hash = digest.digest();
+            String hashCode  = new String(hash);
+            out.println(hashCode);
+
+            // Closes the missing streams
+            out.close();
+            communicationSocket.close();
+
+
+        }catch (Exception e) {e.printStackTrace();}
     }
 
 }
--- src/TCP/Servidor/Servidor.java
+++ src/TCP/Servidor/Servidor.java
@@ -3,6 +3,8 @@ package TCP.Servidor;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 public class Servidor {
@@ -17,26 +19,34 @@ public class Servidor {
             serverSocket = new ServerSocket(port);
             communicationSocket = new ServerSocket(8000);
             System.out.println("Servidor TCP iniciado");
-        }catch (Exception e ){e.printStackTrace();}
 
-        // it opens and selects randomly a file from the folder
-        File folder = new File("src/TCP/Servidor/files");
-        File[] files = folder.listFiles();
-        Random random = new Random();
-        int randomIndex = random.nextInt(files.length);
+            // it opens and selects randomly a file from the folder
+            File folder = new File("src/TCP/Servidor/files");
+            File[] files = folder.listFiles();
+            Random random = new Random();
+            int randomIndex = random.nextInt(files.length);
+
+            // Creates the log entry
+            Date n = new Date();
+            SimpleDateFormat form = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+            String logName = form.format(n) + "-log.txt";
+            File logFile = new File("src/TCP/Servidor/Logs",logName);
+            logFile.createNewFile();
+
 
         // Here, the server accepts the clients and start each connection to a delegate
         while (true) {
-            try{
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
 
-                ServerThread delegate = new ServerThread(clientSocket, threadId, communicationSocket,randomIndex, files, folder);
-                threadId++;
-                delegate.start();
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
 
-            }catch (Exception e) {e.printStackTrace();}
+            ServerThread delegate = new ServerThread(clientSocket, threadId, communicationSocket,randomIndex, files, folder, logFile);
+            threadId++;
+            delegate.start();
+
         }
+
+        }catch (Exception e ){e.printStackTrace();}
 
     }
 }
